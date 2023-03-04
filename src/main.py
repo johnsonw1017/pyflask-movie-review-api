a43e38d1b83d758ef87dd483c198f9cc307c973e
johnsonw1017@gmail.com <johns@192-168-1-100.tpgi.com.au>
2023-03-04 19:33:53 +1000

    configuration file completed

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+db = SQLAlchemy()
+
+def create_app():
+
+    #create the flask app object
+    app = Flask(__name__)
+
+    #configuring the app, includes establishing database URL and secret key
+    app.config.from_object("config.app_config_development") #change to app_config_testing for testing mode
+    
+    #create database object
+    db.init_app(app)
+
+    return app
