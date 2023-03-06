7f7e7a1797db962e4ae78a79e6931fa5bd1005be
johnsonw1017@gmail.com <johns@192-168-1-100.tpgi.com.au>
2023-03-06 21:29:59 +1000

    created the first model Movie

--- src/main.py
+++ src/main.py
@@ -8,6 +8,22 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://db_dev:123456@loc
 #database object
 db = SQLAlchemy(app)
 
+#create database object Movie
+
+class Movie(db.Model):
+  #table name of db
+  __tablename__ = "MOVIES"
+
+  #primary key
+  id = db.Column(db.Integer, primary_key=True)
+
+  #other attributes
+  title = db.Column(db.String())
+  description = db.Column(db.String())
+  director = db.Column(db.String())
+  release_date = db.Column(db.Date())
+  genre = db.Column(db.String()) #this depends whether this data is available
+
 @app.route("/")
 def hello():
   return "Hello World!"
