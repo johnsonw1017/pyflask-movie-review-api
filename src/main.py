b7af598a25d21909d6a1ead45215fcd9063eb0c1
johnsonw1017@gmail.com <johns@192-168-1-100.tpgi.com.au>
2023-03-06 21:18:47 +1000

    database connection established with movie_reveiw_db

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#database URI via SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://db_dev:123456@localhost:5432/movie_review_db"
+
+#database object
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
